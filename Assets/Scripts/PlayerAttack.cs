32ff26ab273d15deca9e45028dffd925a246c47e
Cocomineral <tintinch59@gmail.com>
2025-02-28 19:59:51 -0500

    Added attack cooldown

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -7,12 +7,22 @@ public class PlayerAttack : MonoBehaviour
 {
     [Header("Config")]
     [SerializeField] private Animator _mAnimator;
+    [SerializeField] private float _AttackCooldown;
+
+    private float _internalCooldown;
 
     public void Attack(InputAction.CallbackContext ctx)
     {
-        if (ctx.started)
+        if (ctx.started && _internalCooldown >= _AttackCooldown)
         {
             _mAnimator.Play("attack-melee-right");
+            _internalCooldown = 0;
         }
+    }
+
+    private void Update()
+    {
+        _internalCooldown += Time.deltaTime;
+        _internalCooldown = Mathf.Clamp(_internalCooldown, 0, _AttackCooldown);
     }
 }
