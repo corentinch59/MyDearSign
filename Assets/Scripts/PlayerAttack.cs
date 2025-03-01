bfc870f5a1ff870a7c3c54f9a28eb600b9349588
Cocomineral <tintinch59@gmail.com>
2025-03-01 00:15:04 -0500

    Adjusted attack animation

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -1,28 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.InputSystem;
 
 public class PlayerAttack : MonoBehaviour
 {
     [Header("Config")]
-    [SerializeField] private Animator _mAnimator;
-    [SerializeField] private float _AttackCooldown;
+    [SerializeField] private Animator _animator;
+    [SerializeField] private float _attackCooldown;
+    [SerializeField] private Transform _rollingPin;
 
+    public UnityEvent onAttack;
     private float _internalCooldown;
 
     public void Attack(InputAction.CallbackContext ctx)
     {
-        if (ctx.started && _internalCooldown >= _AttackCooldown)
+        if (ctx.started && _internalCooldown >= _attackCooldown)
         {
-            _mAnimator.Play("attack-melee-right");
+            _animator.Play("attack-melee-right2");
+            onAttack?.Invoke();
             _internalCooldown = 0;
+
         }
     }
 
     private void Update()
     {
         _internalCooldown += Time.deltaTime;
-        _internalCooldown = Mathf.Clamp(_internalCooldown, 0, _AttackCooldown);
+        _internalCooldown = Mathf.Clamp(_internalCooldown, 0, _attackCooldown);
     }
 }
