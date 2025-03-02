d7c1cd5236b5bf63de5963cf0825ff574e026eb7
Théo Le Goc <theo@legoc.net>
2025-03-02 01:46:18 -0500

    Main menuuuuu

---
+++ Assets/Scripts/RandomText.cs
@@ -0,0 +1,17 @@
+using System;
+using TMPro;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    [RequireComponent(typeof(TMP_Text))]
+    public class RandomText : MonoBehaviour
+    {
+        public string[] texts;
+        
+        private void Start()
+        {
+            GetComponent<TMP_Text>().text = texts[UnityEngine.Random.Range(0, texts.Length)];
+        }
+    }
+}
